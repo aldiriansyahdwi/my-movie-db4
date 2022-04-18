9486b930b6bc5fc99e3aee645e7a039acae4c373
Aldiriansyah Dwi <kurosenpai12@gmail.com>
2022-04-18 14:54:57 +0700

    finish design fragment_home and create movie_item design, add function navigate from login to home

--- app/src/main/java/com/example/mymoviedb/LoginFragment.kt
+++ app/src/main/java/com/example/mymoviedb/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.mymoviedb.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -20,5 +21,11 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+    }
 }
