53721f587f41a2abfbf6fe661090b94b8317c5cc
Aldiriansyah Dwi <kurosenpai12@gmail.com>
2022-06-07 17:50:09 +0700

    setup jetpack compose, migrate register and login to compose

--- app/src/main/java/com/example/mymoviedb/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/mymoviedb/ui/login/LoginFragment.kt
@@ -6,76 +6,145 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.datastore.dataStore
-import androidx.datastore.preferences.preferencesDataStore
-import androidx.lifecycle.ViewModelProvider
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.findNavController
 import com.example.mymoviedb.R
-import com.example.mymoviedb.databinding.FragmentLoginBinding
+import com.example.mymoviedb.ui.theme.MyFirstComposeTheme
+import com.example.mymoviedb.ui.theme.OrangeBackground
+import com.example.mymoviedb.ui.theme.OrangeMain
+import com.example.mymoviedb.ui.theme.OrangeSecondary
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-
 class LoginFragment : Fragment() {
-//    private var _binding: FragmentLoginBinding? = null
-//    private val binding get() = _binding!!
+
     private val viewModel: LoginViewModel by viewModel()
-//    private val sharedPreFile = "login_account"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-//        _binding = FragmentLoginBinding.inflate(inflater, container, false)
-//        return binding.root
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(
                 ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
             )
             setContent{
-                MaterialTheme {
-
+                alreadyLogin()
+                MyFirstComposeTheme {
+                    Column{
+                        LoginHeader()
+                        LoginBody(viewModel)
+                        LoginButtonAction(onClickLogin = {
+                            viewModel.apply {
+                                verifyLogin(emailInput.value, passwordInput.value)
+                                user.observe(viewLifecycleOwner){ user->
+                                    if (user.isEmpty()){
+                                        Toast.makeText(
+                                            requireContext(), "account not found", Toast.LENGTH_SHORT).show()
+                                    } else {
+                                        saveDataStore(user[0].email, user[0].username.toString())
+                                        Toast.makeText(requireContext(), "hello ${user[0].username}", Toast.LENGTH_SHORT).show()
+                                        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                                    }
+                                }
+                            }
+                        }, onClickRegister = {
+                            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+                        })
+                    }
                 }
             }
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-//        binding.tvRegister.setOnClickListener {
-//            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
-//        }
-//
-//        binding.btnLogin.setOnClickListener {
-//
-//        }
-//
-    }
-
-    fun alreadyLogin(){
+    private fun alreadyLogin(){
         viewModel.getUsername().observe(viewLifecycleOwner) {
             if (it != "-") {
                 findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
             }
         }
     }
+}
 
-    fun loginAction(){
-//        val email = binding.etEmail.text.toString()
-//        val password = binding.etPassword.text.toString()
-//        viewModel.verifyLogin(email, password)
-//        viewModel.user.observe(viewLifecycleOwner) { user ->
-//            if (user.isEmpty()) {
-//                Toast.makeText(requireContext(), "account not found", Toast.LENGTH_SHORT).show()
-//            } else {
-//                viewModel.saveDataStore(email, user[0].username.toString())
-//                Toast.makeText(requireContext(), "hello ${user[0].username}", Toast.LENGTH_SHORT).show()
-//                findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
-//            }
-//        }
+@Composable
+fun LoginHeader(){
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(modifier = Modifier.heightIn(min = 64.dp, max = 128.dp))
+        Image(painterResource(id = R.drawable.ic_app_main),
+            contentDescription = "",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.widthIn(min = 64.dp, max = 192.dp),
+        )
+    }
+}
+
+@Composable
+fun LoginBody(viewModel: LoginViewModel){
+    val email = viewModel.emailInput.collectAsState()
+    val password = viewModel.passwordInput.collectAsState()
+    Column(modifier = Modifier
+        .padding(horizontal = 48.dp)
+        .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(32.dp))
+        TextField(value = email.value,
+            placeholder = { Text(text = "email")},
+            onValueChange = viewModel::setEmailInput,
+            colors = TextFieldDefaults.textFieldColors(focusedIndicatorColor = OrangeSecondary, backgroundColor = OrangeBackground),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(value = password.value,
+            placeholder = { Text(text = "password")},
+            colors = TextFieldDefaults.textFieldColors(focusedIndicatorColor = OrangeSecondary, backgroundColor = OrangeBackground),
+            onValueChange = viewModel::setPasswordInput,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun LoginButtonAction(onClickLogin :() -> Unit, onClickRegister:() -> Unit){
+    Column(modifier = Modifier
+        .padding(horizontal = 48.dp)
+        .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(modifier = Modifier.fillMaxWidth(), onClick = onClickLogin, colors = ButtonDefaults.buttonColors(backgroundColor = OrangeMain, contentColor = Color.White)) {
+            Text(text = "LOGIN")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Not registered yet?", modifier = Modifier.clickable(onClick = onClickRegister), style = TextStyle(color = Color.Black) )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LoginPreview() {
+    MyFirstComposeTheme {
+        Column {
+            LoginHeader()
+            LoginButtonAction({}, {})
+        }
     }
 }
--- app/src/main/java/com/example/mymoviedb/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/example/mymoviedb/ui/register/RegisterFragment.kt
@@ -8,75 +8,84 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import coil.ImageLoader
 import coil.request.ImageRequest
 import coil.request.SuccessResult
 import com.example.mymoviedb.R
-import com.example.mymoviedb.databinding.FragmentRegisterBinding
 import com.example.mymoviedb.data.userdatabase.User
+import com.example.mymoviedb.ui.theme.*
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RegisterFragment : Fragment() {
 
-    private var _binding: FragmentRegisterBinding? = null
-    private val binding get() = _binding!!
     private val viewModel: RegisterViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.btnRegister.setOnClickListener {
-            val username: String = binding.etUsername.text.toString()
-            val email: String = binding.etEmail.text.toString()
-            val password: String = binding.etPassword.text.toString()
-            val confirmPassword: String = binding.etConfirmPassword.text.toString()
-            checkInput(username, email, password, confirmPassword)
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+            )
+            setContent {
+                MyFirstComposeTheme {
+                    Column {
+                        RegisterHeader()
+                        RegisterBody(viewModel = viewModel)
+                        RegisterButtonAction {
+                            viewModel.apply {
+                                checkInput(
+                                    usernameInput.value,
+                                    emailInput.value,
+                                    passwordInput.value,
+                                    confirmPasswordInput.value
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
-    private fun checkInput(
-        username: String,
-        email: String,
-        password: String,
-        confirmPassword: String
-    ) {
+    private fun checkInput(username: String, email: String, password: String, confirmPassword: String) {
         when {
-            username.length <= 4 -> {
-                binding.etUsername.error = "username must be at least 4 characters"
+            username.isEmpty() -> {
+                Toast.makeText(requireContext(), "please input username", Toast.LENGTH_SHORT).show()
             }
             email.isEmpty() -> {
-                binding.etEmail.error = "email cannot be empty"
+                Toast.makeText(requireContext(), "please input email", Toast.LENGTH_SHORT).show()
             }
-            password.length <= 4 -> {
-                binding.etPassword.error = "password must be at least 4 characters"
+            password.isEmpty() -> {
+                Toast.makeText(requireContext(), "please input password", Toast.LENGTH_SHORT).show()
             }
             confirmPassword != password -> {
-                binding.etConfirmPassword.error = "password doesn't match"
+                Toast.makeText(requireContext(), "password does not match", Toast.LENGTH_SHORT).show()
             }
             else -> {
                 lifecycleScope.launch {
                     viewModel.saveUser(
-                        User(
-                            email,
-                            username,
-                            password,
-                            null,
-                            null,
-                            null,
-                            getBitmap()
-                        )
+                        User(email, username, password, null, null, null, getBitmap())
                     )
                     viewModel.savedUser.observe(viewLifecycleOwner) {
                         if (it != 0.toLong()) {
@@ -104,5 +113,87 @@ class RegisterFragment : Fragment() {
 
         val result = (loading.execute(request) as SuccessResult).drawable
         return (result as BitmapDrawable).bitmap
+    }
+}
+
+@Composable
+fun RegisterHeader(){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Spacer(modifier = Modifier.heightIn(min = 64.dp, max = 128.dp))
+        Text(text = "Register",
+            style = TextStyle(color = Gray, fontSize = 42.sp),
+            modifier = Modifier.padding(start = 48.dp)
+        )
+    }
+}
+
+@Composable
+fun RegisterBody(viewModel: RegisterViewModel) {
+    val email = viewModel.emailInput.collectAsState()
+    val username = viewModel.usernameInput.collectAsState()
+    val password = viewModel.passwordInput.collectAsState()
+    val confirmPassword = viewModel.confirmPasswordInput.collectAsState()
+    Column(modifier = Modifier
+        .padding(horizontal = 48.dp)
+        .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(32.dp))
+        TextField(value = username.value,
+            placeholder = { Text(text = "username")},
+            onValueChange = viewModel::setUsernameInput,
+            colors = TextFieldDefaults.textFieldColors(focusedIndicatorColor = OrangeSecondary, backgroundColor = OrangeBackground),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(value = email.value,
+            placeholder = { Text(text = "email")},
+            onValueChange = viewModel::setEmailInput,
+            colors = TextFieldDefaults.textFieldColors(focusedIndicatorColor = OrangeSecondary, backgroundColor = OrangeBackground),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(value = password.value,
+            placeholder = { Text(text = "password")},
+            colors = TextFieldDefaults.textFieldColors(focusedIndicatorColor = OrangeSecondary, backgroundColor = OrangeBackground),
+            onValueChange = viewModel::setPasswordInput,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(value = confirmPassword.value,
+            placeholder = { Text(text = "confirm password")},
+            colors = TextFieldDefaults.textFieldColors(focusedIndicatorColor = OrangeSecondary, backgroundColor = OrangeBackground),
+            onValueChange = viewModel::setConfirmPasswordInput,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun RegisterButtonAction(onClickRegister:() -> Unit){
+    Spacer(modifier = Modifier.height(32.dp))
+    Button(modifier = Modifier
+        .padding(horizontal = 48.dp)
+        .fillMaxWidth()
+        .height(48.dp),
+        onClick = onClickRegister,
+        colors = ButtonDefaults.buttonColors(backgroundColor = OrangeMain,
+            contentColor = Color.White)
+    ) {
+        Text(text = "REGISTER", fontSize = 18.sp)
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun RegisterPreview() {
+    MyFirstComposeTheme {
+        Column {
+            RegisterHeader()
+//            RegisterBody()
+            RegisterButtonAction{}
+        }
     }
 }
